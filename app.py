933bcdc9f09b0aa46e007b10819579ff9f8be67f
Paul Spengler <132746370+Myandr@users.noreply.github.com>
2024-12-27 16:34:49 +0100

    app.py aktualisieren

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from werkzeug.security import generate_password_hash, check_password_hash
